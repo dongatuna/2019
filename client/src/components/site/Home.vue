74e3de913dd833bda5a81cbe628fc48be408e2ab
dongatuna <ngatuna05@gmail.com>
2019-07-25 16:28:40 -0700

    add relevant information to homepage

--- client/src/components/site/Home.vue
+++ client/src/components/site/Home.vue
@@ -1,14 +1,12 @@
 <template>
-  <section>
-    
+  <section>    
     <main role="main">
-
       <!-- Main jumbotron for a primary marketing message or call to action -->
       <div class="jumbotron">
         <div class="container">
-          <h1 class="display-3">Hello, world!</h1>
-          <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-          <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+          <h1 class="display-3">Welcome to Excel!</h1>
+          <p>Enroll for our classes, prepare for the CNA (HCA) state license exams and get hired by local employers looking for CNAs and caregivers.  Excel Health has trained caregivers in the Puget Sound caregiving community for more than 14 years.  To start your career in the rewarding caregiving profession, enroll in our courses.</p>
+          <p><a class="btn btn-primary btn-lg" href="#" role="button">See our courses &raquo;</a></p>
         </div>
       </div>
 
@@ -16,19 +14,19 @@
         <!-- Example row of columns -->
         <div class="row">
           <div class="col-md-4">
-            <h2>Heading</h2>
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+            <h2>Take a class</h2>
+            <p>We offer CNA, Basic Life Support and CPR/First Aid courses in a variety of schedules: day, evening and weekend. This way, we fit into your schedule rather than the other way round.  Our courses are cost effective and generally short.</p>
+            <p><a class="btn btn-secondary" href="#" role="button">Enroll now &raquo;</a></p>
           </div>
           <div class="col-md-4">
-            <h2>Heading</h2>
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+            <h2>Prepare for exam</h2>
+            <p>Our question bank has over 700 questions.  When you get a question wrong, you not only see what the right answer is but <i>why</i> the right answer is right. In addition, there are professionally made caregiving skill videos for your reference.</p>
+            <p><a class="btn btn-secondary" href="#" role="button">Prep tests &raquo;</a></p>
           </div>
           <div class="col-md-4">
-            <h2>Heading</h2>
-            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+            <h2>Find a job</h2>
+            <p>Our hope is that you will let us be part of your journey to be a healthcare profession.  To that end we invite local employers to our school to present job opportunities to you and ask them to post job opening on our website to shorten your job search.</p>
+            <p><a class="btn btn-secondary" href="#" role="button">Find Jobs &raquo;</a></p>
           </div>
         </div>
 
@@ -37,8 +35,8 @@
 
     <div class="row featurette">
       <div class="col-md-7">
-        <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        <h2 class="featurette-heading">Thanks for sharing with us. <span class="text-muted">We listened.</span></h2>
+        <p class="lead">We listened and will continue doing so - a few employers shared with us the challenges of caregiver shortages and personnel turnover; other employers reviewed and used our website prototype before it went live - for that, we say thank you!  Together, we can craft solutions fit for our community.</p>
       </div>
       <div class="col-md-5">
         <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
@@ -49,8 +47,8 @@
 
     <div class="row featurette">
       <div class="col-md-7 order-md-2">
-        <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        <h2 class="featurette-heading">Find Caregivers. <span class="text-muted">Recruit our graduates.</span></h2>
+        <p class="lead">Some employers often visit our school and present job openings to our students.  Now, you can also create a post about your opening and reach even more caregivers because our website is used by more than our current/former students.</p>
       </div>
       <div class="col-md-5 order-md-1">
         <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
@@ -61,8 +59,8 @@
 
     <div class="row featurette">
       <div class="col-md-7">
-        <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        <h2 class="featurette-heading">CPR/First Aid and BLS certifications<span class="text-muted">Now easy to keep them current.</span></h2>
+        <p class="lead">Your prospective hires and caregivers/nurses require a current CPR/First Aid or BLS certification to work.  We have now made it easy for you to schedule your caregivers for their CPR/FA/BLS renewal/certifications.  </p>
       </div>
       <div class="col-md-5">
         <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
@@ -83,16 +81,16 @@ export default {
 
    metaInfo: {
         title: 'Home page',
-        titleTemplate: 'Home'
-  //       meta: [
-  //           {'http-equiv': 'Content-Type', content: 'text/html; charset=utf-8'},
-  //           {name: 'viewport'. content: 'width=device-width, initial-scale=1'},
-  //           {name: 'description', content: 'This is the homepage of Excel Health Careers Training'}
-  //       ],
+        titleTemplate: 'Home',
+        meta: [
+            {'http-equiv': 'Content-Type', content: 'text/html; charset=utf-8'},
+            {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+            {name: 'description', content: 'This is the homepage of Excel Health Careers Training'}
+        ],
 
-  //       links: [
-  //           {rel: 'canonical', href: 'https://www.goexcelcna.com'}
-  //       ]
+        links: [
+            {rel: 'canonical', href: 'https://www.goexcelcna.com'}
+        ]
     },
  
 }
